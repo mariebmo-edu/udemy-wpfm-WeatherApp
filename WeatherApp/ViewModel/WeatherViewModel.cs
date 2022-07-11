d9ffdfd3f7364eb8025666551bf431706fd272dd
mariestigen <marie.stigen@hotmail.com>
2022-07-11 19:20:38 +0200

    Add project files.

---
+++ WeatherApp/ViewModel/WeatherViewModel.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherApp.Annotations;
+using WeatherApp.Model;
+
+namespace WeatherApp.ViewModel
+{
+    internal class WeatherViewModel : INotifyPropertyChanged
+    {
+
+        public string query;
+
+        public string Query
+        {
+            get { return query;}
+            set
+            {
+                query = value;
+                OnPropertyChanged("Query");
+            }
+        }
+
+        private CurrentConditions currentConditions;
+
+        public CurrentConditions CurrentConditions
+        {
+            get { return currentConditions; }
+            set
+            {
+                currentConditions = value;
+                OnPropertyChanged("CurrentConditions");
+            }
+        }
+
+        private City selectedCity { get; set; }
+
+        public City SelectedCity
+        {
+            get { return selectedCity; }
+            set
+            {
+                selectedCity = value;
+                OnPropertyChanged("SelectedCity");
+            }
+        }
+
+        public WeatherViewModel()
+        {
+            SelectedCity = new City
+            {
+                LocalizedName = "Oslo"
+            };
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
